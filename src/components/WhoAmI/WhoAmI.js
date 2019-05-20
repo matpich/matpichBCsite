604e435542e3c7677589079a1f9e4f7dfd821b65
Mateusz Pichniarczyk <mat.pich@interia.pl>
2019-05-20 21:55:24 +0200

    Add WhoAmI Mobile section

--- src/components/WhoAmI/WhoAmI.js
+++ src/components/WhoAmI/WhoAmI.js
@@ -1,14 +1,54 @@
 import React from "react";
 
-
+import "./WhoAmI.css"
 
 
 class WhoAmI extends React.Component {
     render() {
         return ( 
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus venenatis turpis, eget suscipit urna mattis non. Praesent auctor magna ac leo porttitor rhoncus commodo sed tellus. Curabitur non turpis pulvinar, tincidunt est vitae, dictum turpis. Duis vitae blandit felis. Fusce sollicitudin odio lorem, faucibus egestas justo vulputate vel. Nulla facilisi. Sed sed iaculis eros. In iaculis id ligula quis iaculis. Quisque mattis id diam et consequat. Morbi eget tellus accumsan, ullamcorper dui vel, gravida ante. Fusce rutrum velit interdum massa lacinia semper. Morbi in velit sed nisl mattis interdum nec non odio. Mauris a metus id felis condimentum mattis. Aliquam erat volutpat. Morbi sollicitudin orci sapien, non suscipit arcu auctor quis. Aliquam et scelerisque velit.</p>
-            </div>
+            <section className="who-am-i">
+                <h1 className="section-title">Kim jestem?</h1>
+
+                <p className="describe">Lorem, ipsum dolor sit amet consectetur adipisicing elit.Recusandae ex debitis reprehenderit tempore est itaque velit laudantium error ? Dolorum illum repudiandae ad ? Illum quaerat sapiente veniam ut corporis dolorum sunt, distinctio recusandae incidunt vero iusto accusantium.Illo maxime quasi dignissimos, similique repellat facilis provident dicta eaque at, fugit facere suscipit.</p>
+            
+                <div className="photo-container">
+                    <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="kitten"></img>
+                </div>
+
+                <div className="details">
+                    <h3 className="section-subtitle">Dane</h3>
+
+                    <div className="detail">
+                        <h5>Dane osobowe:</h5>
+                        <p>Mateusz Pichniarczyk</p>
+                    </div>
+
+                    <div className="detail">
+                        <h5>Wiek:</h5>
+                        <p>27 lat</p>
+                    </div>      
+
+                    <div className="detail">
+                        <h5>Lokalizacja:</h5>
+                        <p>Wrocław, Polska</p>
+                    </div>   
+
+                    <div className="detail">
+                        <h5>Telefon:</h5>
+                        <p><span id="prefix">+ 48 </span>663 625 037</p>
+                    </div>      
+
+                    <div className="detail">
+                        <h5>E-mail:</h5>
+                        <p>mat.pich@interia.pl</p>
+                    </div>       
+
+                    <div className="detail" id="detail-social">
+                        <i className="fab fa-github-square"></i>
+                        <i className="fab fa-linkedin"  style={{color:'#0e76a8'}}> </i>
+                    </div>  
+                </div>
+            </section>
         )
     }
 }
